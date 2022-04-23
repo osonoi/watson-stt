fcd5fdd3d1d0dd9d5811760a3f101d3e29bdbaae
小薗井康志 <shousonoiyasushi@shousonoiyasushinoMacBook-Pro.local>
2022-04-23 13:13:16 +0900

    change test

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,6 @@
 // sum.test.js
 const sum = require('./sum');
 
-test('adds 2 + 2 to equal 3', () => {
-  expect(sum(2, 2)).toBe(3);
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });
